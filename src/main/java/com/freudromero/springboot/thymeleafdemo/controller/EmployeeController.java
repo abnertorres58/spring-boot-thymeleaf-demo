412c2472b2cbb6431781f18d8f6ab4277c317037
fred <abnertorres58@gmail.com>
2022-05-05 00:17:46 -0400

    Spring Boot - Thymeleaf - CRUD Database Real-Time Project: Add Employee

--- src/main/java/com/freudromero/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/freudromero/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -33,6 +33,27 @@ public class EmployeeController {
         // Add to the spring model
         theModel.addAttribute("employees", theEmployees);
 
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        // Create the model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+        // Save the employee
+        employeeService.save(theEmployee);
+
+        // Use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+
     }
 }
--- src/main/java/com/freudromero/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/freudromero/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,10 +3,14 @@ package com.freudromero.springboot.thymeleafdemo.dao;
 import com.freudromero.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-	// that's it ... no need to write any code LOL!
+    // Add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+
+
 	
 }
